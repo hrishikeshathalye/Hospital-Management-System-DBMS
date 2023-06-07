61d81b4b6fa1ef0f1f164d34d7eda0c9684b3ddb
Mad0squirrel <eghorkin@gmail.com>
2023-06-07 15:15:51 +0300

    add tables

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ var path = require('path');
 // var logger = require('morgan');
 var mysql = require('mysql');
 var cors = require('cors');
-var port = 3301
+var port = 3001
 
 //Connection Info
 var con = mysql.createConnection({
@@ -17,9 +17,99 @@ var con = mysql.createConnection({
 });
 
 //Connecting To Database
-con.connect(function (err) {
+con.connect((err) => {
   if (err) throw err;
-  console.log("Connected to MySQL");
+  console.log('Соединение с базой данных установлено!');
+
+  // const queries = [
+  //   `CREATE TABLE Patient(
+  //     email varchar(50) PRIMARY KEY,
+  //     password varchar(30) NOT NULL,
+  //     name varchar(50) NOT NULL,
+  //     address varchar(60) NOT NULL,
+  //     gender VARCHAR(20) NOT NULL
+  //     );`,
+  //   `CREATE TABLE MedicalHistory(
+  //     id int PRIMARY KEY,
+  //     date DATE NOT NULL,
+  //     conditions VARCHAR(100) NOT NULL, 
+  //     surgeries VARCHAR(100) NOT NULL, 
+  //     medication VARCHAR(100) NOT NULL
+  //     );`,
+  //   `CREATE TABLE Doctor(
+  //     email varchar(50) PRIMARY KEY,
+  //     gender varchar(20) NOT NULL,
+  //     password varchar(30) NOT NULL,
+  //     name varchar(50) NOT NULL
+  //     );`,
+  //   `CREATE TABLE Appointment(
+  //     id int PRIMARY KEY,
+  //     date DATE NOT NULL,
+  //     starttime TIME NOT NULL,
+  //     endtime TIME NOT NULL,
+  //     status varchar(15) NOT NULL
+  //     );`,
+  //   `CREATE TABLE PatientsAttendAppointments(
+  //     patient varchar(50) NOT NULL,
+  //     appt int NOT NULL,
+  //     concerns varchar(40) NOT NULL,
+  //     symptoms varchar(40) NOT NULL,
+  //     FOREIGN KEY (patient) REFERENCES Patient (email) ON DELETE CASCADE,
+  //     FOREIGN KEY (appt) REFERENCES Appointment (id) ON DELETE CASCADE,
+  //     PRIMARY KEY (patient, appt)
+  //     );`,
+  //   `CREATE TABLE Schedule(
+  //     id int NOT NULL,
+  //     starttime TIME NOT NULL,
+  //     endtime TIME NOT NULL,
+  //     breaktime TIME NOT NULL,
+  //     day varchar(20) NOT NULL,
+  //     PRIMARY KEY (id, starttime, endtime, breaktime, day)
+  //     );`,
+  //   `CREATE TABLE PatientsFillHistory(
+  //     patient varchar(50) NOT NULL,
+  //     history int NOT NULL,
+  //     FOREIGN KEY (patient) REFERENCES Patient (email) ON DELETE CASCADE,
+  //     FOREIGN KEY (history) REFERENCES MedicalHistory (id) ON DELETE CASCADE,
+  //     PRIMARY KEY (history)
+  //     );`,
+  //   `CREATE TABLE Diagnose(
+  //     appt int NOT NULL,
+  //     doctor varchar(50) NOT NULL,
+  //     diagnosis varchar(40) NOT NULL,
+  //     prescription varchar(50) NOT NULL,
+  //     FOREIGN KEY (appt) REFERENCES Appointment (id) ON DELETE CASCADE,
+  //     FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
+  //     PRIMARY KEY (appt, doctor)
+  //     );`,
+  //   `CREATE TABLE DocsHaveSchedules(
+  //     sched int NOT NULL,
+  //     doctor varchar(50) NOT NULL,
+  //     FOREIGN KEY (sched) REFERENCES Schedule (id) ON DELETE CASCADE,
+  //     FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
+  //     PRIMARY KEY (sched, doctor)
+  //     );`,
+  //   `CREATE TABLE DoctorViewsHistory(
+  //     history int NOT NULL,
+  //     doctor varchar(50) NOT NULL,
+  //     FOREIGN KEY (doctor) REFERENCES Doctor (email) ON DELETE CASCADE,
+  //     FOREIGN KEY (history) REFERENCES MedicalHistory (id) ON DELETE CASCADE,
+  //     PRIMARY KEY (history, doctor)
+  //     );`
+  //   // Добавьте здесь другие запросы, если необходимо
+  // ];
+
+  // queries.forEach((query) => {
+  //   con.query(query, (err, result) => {
+  //     if (err) throw err;
+  //     console.log('Запрос выполнен успешно:', query);
+  //   });
+  // });
+
+  // con.end((err) => {
+  //   if (err) throw err;
+  //   console.log('Соединение с базой данных закрыто!');
+  // });
 });
 
 
